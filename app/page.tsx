0c632ddcf706e010ba0a21b1721152f375c907c7
Yash Kumar <yashk9293@gmail.com>
2024-12-01 04:08:57 +0530

    background

--- app/page.tsx
+++ app/page.tsx
@@ -13,12 +13,13 @@ export default function Home() {
   };
 
   return (
-    <main className="min-h-screen flex flex-col items-center justify-center gap-8">
-      <h1 className="font-black text-green-600 text-3xl">Chessify</h1>
+    <main className="min-h-screen flex flex-col items-center justify-center gap-8 bg-chess-pattern bg-cover bg-center">
+      <h1 className="font-black text-green-700 text-5xl sm:text-6xl">Chessify</h1>
       <div className="flex justify-center items-center gap-4">
         <Button
           color="success"
           variant="ghost"
+          className="text-white bg-green-700 hover:bg-green-700 text-lg py-3 px-6 rounded-lg"
           onClick={(e) => {
             e.preventDefault();
             openModal("computer");
